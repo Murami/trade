9460c4c6e4fe5dbf61153648f39b4ab30977802f
guerot_a <guerot_a@epitech.eu>
2014-06-07 23:18:06 +0200

    .

--- trade.py
+++ trade.py
@@ -2,10 +2,17 @@
 
 import sys
 
+factor_main = 1
+factor_sell = 0.1
+factor_buy = 0.0001
 capital = 0
+capital_start = 0
 nb_days = 0
+nb_days_total = 0
 stocks_prices = []
 stocks = 0
+
+## AU DERNIER JOUR ON RENVEND TOUTES NOS ACTIONS
 
 def avg(l):
     return sum(l) / float(len(l))
@@ -29,6 +36,7 @@ def buy_stock(stock_price, nb_stock):
 
     capital = capital - calc_buy_outcome(stock_price, nb_stock)
     stocks = stocks + nb_stock
+    print("buy " + str(nb_stock))
 #buy_stock#
 
 def sell_stock(stock_price, nb_stock):
@@ -37,42 +45,64 @@ def sell_stock(stock_price, nb_stock):
 
     capital = capital + calc_sell_income(stock_price, nb_stock)
     stocks = stocks - nb_stock
+    print("sell " + str(nb_stock))
 #sell_stock#
 
 def wait():
     print("wait")
 #wait#
 
-def sell_actions(stick_price, avg_tot, avg_100, avg_50, avg_25, avg_10):
-    print("sell do nothing for the moment")
+def safe_sell(stock_price, nb_stock):
+    global capital
+    global stocks
+
+    if (ammount == 0):
+        wait()
+    else:
+        commision_unit = 0.15 * float(stock_price)
+        sell_capacity = int(float(capital) / float(commision_unit))
+        
+#safe_sell#
+
+def safe_buy(stock_price, nb_stock):
+    global capital
+    global stocks
+#safe_buy#
+
+def sell_actions(stock_price, avg_price):
+    variation = float(abs(avg_price - stock_price)) / float(avg_price)
+    factor = 1 - float(nb_days) / float(nb_days_total)
+    ammount = int(factor_main * factor_sell * factor * variation * capital_start)
+    safe_sell(stock_price, ammount)
 #sell_actions#
 
-def buy_action(stick_price, avg_tot, avg_100, avg_50, avg_25, avg_10):
-    print("buy do nothing for the moment")
+def buy_actions(stock_price, avg_price):
+    variation = float(abs(avg_price - stock_price)) / float(avg_price)
+    factor = float(nb_days) / float(nb_days_total)
+    ammount = int(factor_main * factor_buy * factor * variation * capital * capital_start)
+    safe_buy(stock_price, ammount)
 #buy_actions#
 
 def initialize():
     global capital
     global nb_days
+    global nb_days_total
+    global capital_start
 
     capital = int(sys.stdin.readline())
+    capital_start = capital
     nb_days = int(sys.stdin.readline())
+    nb_days_total = nb_days
 #initialize#
 
 def make_action(stock_price):
     global stocks_prices;
 
-    avg_tot = avg(stock_prices)
-    avg_100 = avg(stock_prices[-100:])
-    avg_50 = avg(stock_prices|-50:])
-    avg_25 = avg(stock_prices[-25:])
-    avg_10 = avg(stock_prices|-10:])
-    # if (avg_tot > avg_100 && avg_100 > avg_50 && avg_50 > avg_25 && avg_25 > avg_10):
-    #     ;## crack boursier
-    if (stock_price > avg):
-        sell_actions(stock_price, avg_tot)
-    elif (stock_price < avg):
-        buy_actions(stock_price, avg_tot)
+    avg_price = avg(stocks_prices[-50:])
+    if (stock_price > avg_price):
+        sell_actions(stock_price, avg_price)
+    elif (stock_price < avg_price):
+        buy_actions(stock_price, avg_price)
     else:
         wait();
 #make_action#
@@ -87,6 +117,7 @@ def trade():
     while nb_days > 0:
         stock_price = int(sys.stdin.readline()[:-1])
         stocks_prices.append(stock_price)
+        make_action(stock_price)
         nb_days = nb_days - 1
 #trade#
 trade()
